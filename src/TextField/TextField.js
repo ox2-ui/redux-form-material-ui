869cd247c94f3d65abb1cdc0debe9d68c4b08700
Lauricio Su <me@lauricio.com>
2017-02-26 19:19:59 +0700

    [Checkbox] use ox2 material ui

--- src/TextField/TextField.js
+++ src/TextField/TextField.js
@@ -1,27 +1,5 @@
 import React, { PropTypes } from 'react';
-import { default as MaterialTextField } from 'material-ui/TextField';
-
-const styles = {
-  error: {
-    color: 'hsla(6, 63%, 46%, 1)',
-  },
-  underline: {
-    borderColor: 'hsla(214, 70%, 53%, 1)',
-  },
-  label: {
-    color: 'hsla(0, 0%, 35%, 1)',
-  },
-  labelFocus: {
-    color: 'hsla(214, 70%, 53%, 1)',
-  },
-  hint: {
-    color: 'hsla(0, 0%, 67%, 1)',
-    fontFamily: 'Roboto Condensed',
-  },
-  input: {
-    fontFamily: 'Roboto Condensed',
-  },
-};
+import { default as MaterialTextField } from '@ox2/material-ui/TextField';
 
 /**
  * TextField Component
@@ -29,15 +7,7 @@ const styles = {
 const TextField = ({ input, label, meta: { touched, error }, ...custom }) => (
   <MaterialTextField
     floatingLabelText={label}
-    floatingLabelFixed={true}
-    underlineFocusStyle={styles.underline}
-    floatingLabelStyle={styles.label}
-    floatingLabelFocusStyle={touched && error ? styles.error : styles.labelFocus}
-    errorStyle={styles.error}
-    inputStyle={styles.input}
-    hintStyle={styles.hint}
     errorText={touched && error}
-    style={{fontSize: '18px', width: '100%'}}
     {...input}
     {...custom}
   />
